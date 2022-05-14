4a44a90cb90723043a463a76e4c638b1493949f1
ariyan1985 <nassirariyan85@gmail.com>
2022-05-14 09:11:38 -0700

    New Update

---
+++ src/gitclasses/Class03/Main.java
@@ -0,0 +1,13 @@
+package gitclasses.Class03;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Hello Hello");
+        System.out.println("Hello Batch 12");
+        System.out.println("Hello Best Batch Everrrrr!!!!!     LOL");
+
+        
+
+
+    }
+}
